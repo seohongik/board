75e8e15f2239d2a74eaf39d4f7d8c55563f97ec3
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-10-02 12:32:57 +0900

    Update board_item_list.js

--- src/main/webapp/resources/js/board_item_list.js
+++ src/main/webapp/resources/js/board_item_list.js
@@ -3,12 +3,10 @@
  */
  
  $(function(){
-		let hasFile =document.querySelectorAll('.hasFile');
-		  
-		for(let i=0; i<hasFile.length; i++){
-	
-			if(hasFile[i].textContent==='Y'){
-				  hasFile[i].style.color ='red'
-			}
+	let hasFile =document.querySelectorAll('.hasFile');
+	for(let i=0; i<hasFile.length; i++){
+		if(hasFile[i].textContent==='Y'){
+			  hasFile[i].style.color ='red'
 		}
-	});
+	}
+});
