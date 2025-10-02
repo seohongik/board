4af2691bb3ec17acddddce8090930b3b0694bb06
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-10-02 12:30:49 +0900

    Update board_item_file_upload_in_writer_with_file.js

--- src/main/webapp/resources/js/board_item_file_upload_in_writer_with_file.js
+++ src/main/webapp/resources/js/board_item_file_upload_in_writer_with_file.js
@@ -6,77 +6,51 @@ const handler = {
     init() {
         const fileInput = document.querySelector('#file-input');
         const preview = document.querySelector('#preview');
-
         fileInput.addEventListener('change', () => {
-
             let uploadTime = (Date.now() + ".000");
-
-            console.log(uploadTime);
-
-
             const files = Array.from(fileInput.files)
             files.forEach(file => {
-
                 formDataUploadTime.append(file.lastModified, uploadTime);
-
                 preview.innerHTML += `
                 <p id="${file.lastModified}">
                     ${file.name}
                     <button data-index='${file.lastModified}' class='file-remove'>X</button>
                 </p>`;
-
-
                 formDataSend.append(file.lastModified, file);
             });
-
-
         });
-
     },
 
     removeFile: () => {
-
         document.addEventListener('click', (e) => {
             if (e.target.className !== 'file-remove') return;
-
             let removeTargetId = e.target.dataset.index;
             const removeTarget = document.getElementById(removeTargetId);
             const files = document.querySelector('#file-input').files;
             const dataTransfer = new DataTransfer();
-
-
+            
             Array.from(files)
                 .filter(file => file.lastModified != removeTargetId)
                 .forEach(file => {
                     dataTransfer.items.add(file);
                 });
-
-
+            
             let lastModified = removeTargetId;
             formDataDel.append('deleteFiles', lastModified);
-
             formDataUploadTime.delete(lastModified);
-
             document.querySelector('#file-input').files = dataTransfer.files;
             formDataSend.delete(formDataDel.getAll('deleteFiles'));
-
-
             removeTarget.remove(lastModified);
         })
-
     },
-
 }
 
 handler.init();
 handler.removeFile();
 
 $(function () {
-
     $('#submitbtn').on("click", function () {
-
         const realSendFormDataWithFile = new FormData();
-
         const jsonData = {
             "writerName": $('#writerName').val(),
             "updatedWhen": $('#updatedWhen').val(),
@@ -85,11 +59,9 @@ $(function () {
             "id": Number($('#id').val()),
             "userId": $('#userId').val()
         }
-
         Object.entries(jsonData).forEach(item => realSendFormDataWithFile.append(item[0], item[1])); /*textDataAdd*/
-
         const objFileData = {};
-
+        
         formDataSend.forEach((value, key) => {
             objFileData[key] = value
             realSendFormDataWithFile.append('file', value);
@@ -99,9 +71,7 @@ $(function () {
             objFileData[key] = value
             realSendFormDataWithFile.append('uploadTime', value);
         });
-
-
-
+        
         $.ajax({
             url: "/board/insertBoardDataWithFile",
             type: "POST",
@@ -120,7 +90,5 @@ $(function () {
                 alert(error.responseJSON.resDescription);
             }
         });
-
     });
-
 });
