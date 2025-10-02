b7cd3f9fab5ab5365aa2091359fbc166ea362c0a
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-10-02 12:32:27 +0900

    Update board_item_file_upload_in_update_with_file.js

--- src/main/webapp/resources/js/board_item_file_upload_in_update_with_file.js
+++ src/main/webapp/resources/js/board_item_file_upload_in_update_with_file.js
@@ -8,58 +8,42 @@ const formDataDel = new FormData();
 const formDataUploadTime = new FormData();
 const handler = {
     init() {
-
         const fileInput = document.querySelector('#file-input');
         const preview = document.querySelector('#preview');
-
+        
         fileInput.addEventListener('change', () => {
-
             if (preview.innerText === "파일 없음") {
                 let noFile = preview.innerText;
                 preview.innerText = noFile.replace(noFile, "");
             }
-
             let uplodeTime = Date.now() + ".000"
-
             const files = Array.from(fileInput.files)
-
+            
             files.forEach(file => {
                 preview.innerHTML += `
                 <p id="${file.lastModified}" class="files">
                     ${file.name}
                     <button data-index='${file.lastModified}'  class='file-remove'>X</button>
                 </p>`;
-
                 formDataUploadTime.append(file.lastModified, uplodeTime);
-
                 formDataSend.append(file.lastModified, file)
-
             });
-
         });
-
-
     },
     removeFile: () => {
-
         document.addEventListener('click', (e) => {
-
             if (e.target.className !== 'file-remove') return;
             let removeTargetId = e.target.dataset.index;
             const removeTarget = document.getElementById(removeTargetId);
             const files = document.querySelector('#file-input').files;
             const dataTransfer = new DataTransfer();
-
-
+            
             Array.from(files)
                 .filter(file => file.lastModified != removeTargetId)
                 .forEach(file => {
                     dataTransfer.items.add(file);
                 });
-
-
             document.querySelector('#file-input').files = dataTransfer.files;
-
             let delId = removeTargetId;
             removeTarget.remove(delId);
 
@@ -68,11 +52,8 @@ const handler = {
 
             let fileMeta = e.target.value;
             formDataDel.append('deleteFileCondition', fileMeta);
-
         })
-
     },
-
 }
 
 
@@ -80,23 +61,19 @@ handler.init();
 handler.removeFile();
 
 $(function () {
-
     let hasFile = document.querySelectorAll(".fileLink");
     let preveiwFiles = document.querySelector("#preview");
-
     if (hasFile.length === 0) {
-
         let noFile = preveiwFiles.innerText;
         preveiwFiles.innerText = noFile.replace(noFile, "파일 없음");
     }
-
+    
     $("#toList").on("click", function () {
         let pageNum = $('#pageNum').val();
         location.href = '/board/showAllList?pageNum=' + pageNum
     });
-
+    
     $('#updateBtn').on("click", function () {
-
         const jsonData = {
             "writerName": $('#writerName').val(),
             "updatedWhen": $('#updatedWhen').val(),
@@ -104,28 +81,22 @@ $(function () {
             "content": $('#content').val(),
             "id": Number($('#id').val()),
             "userId": $('#userId').val(),
-
         }
         const realSendFormDataWithFile = new FormData();
-
         Object.entries(jsonData).forEach(item => realSendFormDataWithFile.append(item[0], item[1]));
-
+        
         //updateFiles
-
         formDataSend.forEach((key, value) => {
             realSendFormDataWithFile.append('updateFiles', key);
         });
-
-
+        
         formDataUploadTime.forEach((key, value) => {
-
             realSendFormDataWithFile.append('uploadTime', key);
         });
 
         formDataDel.forEach((key, value) => {
             realSendFormDataWithFile.append('deleteFileCondition', key);
         });
-
 
         $.ajax({
             url: "/board/updateBoardDataWithFile",
@@ -136,19 +107,15 @@ $(function () {
             success: function (result) {
                 if (result.code === 200) {
                     alert("글 작성이 완료되었습니다.");
-
                     let pageNum = $("#pageNum").val();
-
                     location.href = '/board/showAllList?pageNum=' + pageNum;
                 }
             },
             error: function (error) {
-
                 //400 에러만 처리 나중에 코드가 생길시 더 추가
                 alert(error.responseJSON.resDescription);
-
             }
         });
-
     });
 });
+
